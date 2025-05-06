6f8e3da51954129504c6d8f259e812a9c15a033f
andrewliden <47042544+andrewliden@users.noreply.github.com>
2025-05-06 16:43:02 -0400

    Breaking things up a bit, some visual stuff.

--- next-cosplanner/next-frontend/src/app/cosplays/page.tsx
+++ next-cosplanner/next-frontend/src/app/cosplays/page.tsx
@@ -1,24 +1,34 @@
 import type Cosplay from "@/type-definitions/Cosplay";
 import { HOSTED_AT } from "@/env";
-import PageHeading from "@/components/PageHeading";
+import ContentBox from "@/components/ContentBox";
+import CreateNewCosplay from "@/components/CreateNewCosplay";
+
+function ListCosplay({data}: {data: Cosplay}) {
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    );
+}
 
 export default async function CosplaysPage() {
     const req = await fetch(`${HOSTED_AT}/api/cosplay`);
     const j = await req.json() as Cosplay[];
     return (
-        <div>
-            <PageHeading>
-                Your cosplays
-            </PageHeading>
-            {j.length === 0 ?
-                <p>It looks like you don&apos;t have any cosplays yet.</p>
-                :
-                j.map(cosplay => 
-                    <div key={cosplay.id}>
-                        {JSON.stringify(cosplay)}
-                    </div>
-                )
-            }
-        </div>
+        <ContentBox label='My cosplays'>
+            <CreateNewCosplay />
+            <div>
+                {j.length === 0 ?
+                    <p>It looks like you don&apos;t have any cosplays yet.</p>
+                    :
+                    j.map(cosplay => 
+                        <ListCosplay
+                            data={cosplay}
+                            key={cosplay.id}
+                        />
+                    )
+                }
+            </div>
+        </ContentBox>
     );
 }
--- next-cosplanner/next-frontend/src/components/ContentBox.tsx
+++ next-cosplanner/next-frontend/src/components/ContentBox.tsx
@@ -1,6 +1,9 @@
-export default function ContentBox({children}: {children?: React.ReactNode}) {
+import PageHeading from "./PageHeading";
+export default function ContentBox({children, label}: {children?: React.ReactNode, label: React.ReactNode}) {
     return(
-        <div className='bg-'>
+        <div className='bg-gray-900 p-2 rounded-lg'>
+            <PageHeading>{label}</PageHeading>
+            <hr />
             {children}
         </div>
     );
