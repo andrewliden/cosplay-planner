bd77b01d2b678c0c789cdd36d96a9e8695a7fc7b
andrewliden <47042544+andrewliden@users.noreply.github.com>
2025-06-03 16:31:38 -0400

    Added an incomplete create cosplay part mutation field

--- django-orm/cosplayplanner/schemas/mutation_schema.py
+++ django-orm/cosplayplanner/schemas/mutation_schema.py
@@ -1,5 +1,5 @@
 import graphene
-from cosplays.models import Cosplay
+from cosplays.models import Cosplay, CosplayPart
 from cosplayplanner.schemas.schema_types import CosplayType
 
 class CreateCosplayMutation(graphene.Mutation):
@@ -18,6 +18,33 @@ class CreateCosplayMutation(graphene.Mutation):
         created.save()
         return CreateCosplayMutation(cosplay=created)
 
+class CreateCosplayPartMutation(graphene.Mutation):
+    class Arguments:
+        cosplay_id = graphene.String(required=True)
+        name = graphene.String(required=True)
+        description = graphene.String(required=True)
+        build_steps = graphene.String()
+        time_estimate = graphene.String()
+        store_link = graphene.String()
+
+    cosplay_part = graphene.Field(CosplayPart)
+
+    @classmethod
+    def mutate(cls, root, info, **kwargs):
+        #TODO - Read the docs a little more,
+        # Find out if this is actually a good way to handle non-required fields,
+        # and stuff with foreign key relations
+        # Also not sure you can unpack kwargs like that in this library
+        created = CosplayPart.objects.create(
+            cosplay_id = kwargs['cosplay_id'],
+            name=kwargs.get['name'],
+            description=kwargs['description'],
+            build_steps=kwargs.get('build_steps'),
+            time_estimate=kwargs.get('time_estimate')
+
+        )
+        created.save()
+        return CreateCosplayPartMutation(cosplay_part=created)
 
 class Mutation(graphene.ObjectType):
     create_cosplay = CreateCosplayMutation.Field()
